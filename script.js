24daf6fa7af54bc13dae746042198a2acd99ce3b
Rustam <189756814+rustleium@users.noreply.github.com>
2025-01-18 09:28:14 +0530

    feat: add function to generate random RGB values

--- script.js
+++ script.js
@@ -109,4 +109,11 @@ function increaseOpacity () {
   });
 }
 
+function getRandomRGB() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
 startEtching();
